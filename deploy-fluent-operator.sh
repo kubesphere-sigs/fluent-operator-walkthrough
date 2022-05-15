1388f8662fe2c25edfb60b8d3e20c77e7f1e156d
Benjamin Huo <benjamin@kubesphere.io>
2022-05-15 16:49:42 +0800

    increase install timeout & remove manifests
    Signed-off-by: Benjamin Huo <benjamin@kubesphere.io>

--- deploy-fluent-operator.sh
+++ deploy-fluent-operator.sh
@@ -9,8 +9,8 @@ if [[ "${INSTALL_HELM:-no}" == "yes" ]]; then
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 fi
 
-helm upgrade --install fluent-operator --create-namespace -n "$LOGGING_NAMESPACE" --wait --timeout 60s https://github.com/fluent/fluent-operator/releases/download/v1.0.1/fluent-operator.tgz
+helm upgrade --install fluent-operator --create-namespace -n "$LOGGING_NAMESPACE" --wait --timeout 10m https://github.com/fluent/fluent-operator/releases/download/v1.0.1/fluent-operator.tgz
 
 echo -e "\n"
-kubectl -n "$LOGGING_NAMESPACE" wait --for=condition=available deployment/fluent-operator --timeout=60s
+kubectl -n "$LOGGING_NAMESPACE" wait --for=condition=available deployment/fluent-operator --timeout=10m
 echo "Please visit https://github.com/fluent/fluent-operator/tree/master/manifests/fluentd to apply the manifests that you want to explore."
