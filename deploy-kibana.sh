fff05a6bb3112a083411072b3e931278bc00f147
chengdehao <dehaocheng@kubesphere.io>
2022-05-16 15:38:13 +0800

    Separate out kibana and es installation
    Signed-off-by: chengdehao <dehaocheng@kubesphere.io>

--- deploy-kibana.sh
+++ deploy-kibana.sh
@@ -12,5 +12,9 @@ fi
 helm repo add elasticsearch https://helm.elastic.co || helm repo add elasticsearch https://helm.elastic.co/
 helm repo update
 
-helm upgrade --install --namespace="$ES_NAMESPACE" --create-namespace --wait elasticsearch elasticsearch/elasticsearch \
-  --set replicas=1,minMasterNodes=1
+
+# Kibana is provided primarily for visualisation
+helm upgrade --install --namespace="$ES_NAMESPACE" --create-namespace --wait kibana elasticsearch/kibana
+
+echo "Kibana deployed, now set up indexes by going to http://localhost:5601/app/management/kibana/indexPatterns after running:"
+echo "kubectl port-forward -n elastic svc/kibana-kibana 5601"
